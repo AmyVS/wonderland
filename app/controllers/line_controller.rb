41466143fda9b1f3ee2f3c799b9c7e1032045f1b
Aoede Pando & Amy Vaillancourt-Sals <student@epicodus.com>
2014-09-02 12:24:02 -0700

    add CRUD to station and line

---
+++ app/controllers/line_controller.rb
@@ -0,0 +1,46 @@
+class Line < ApplicationController
+
+  def index
+    @lines = Lines.all
+  end
+
+  def new
+    @line = Line.new
+  end
+
+  def create
+    @line = Line.create(params[:line])
+    if @line.valid?
+      flash[:notice] = "Line created."
+      redirect_to lines_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @line = Line.find(params[:id])
+  end
+
+  def edit
+    @line = Line.find(params[:id])
+  end
+
+  def update
+    @line = Line.find(params[:id])
+    if line.update(params[:line])
+      flash[:notice] = "Line updated."
+      redirect_to line_path(@line)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @line = Line.find(params[:id])
+    @line.destroy
+    flash[:notice] = "Line deleted."
+    redirect_to line_path
+  end
+
+end
--- app/controllers/station_controller.rb
+++ app/controllers/station_controller.rb
@@ -4,4 +4,43 @@ class StationsController < ApplicationController
     @stations = Station.all
   end
 
+  def new
+    @station = Station.new
+  end
+
+  def create
+    @stations = Station.new(params[:stations])
+    if @stations.save
+      flash[:notice] = "Station created."
+      redirect_to stations_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @station = Station.find(params[:id])
+  end
+
+  def edit
+    @station = Station.find(params[:id])
+  end
+
+  def update
+    @station = Station.find(params[:id])
+    if @station.update(params[:station])
+      flash[:notice] = "Station updated."
+      redirect_to station_path(@station)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @station = Station.find(params[:id])
+    @station.destroy
+    flash[:notice] = "Station deleted."
+    redirect_to contacts_path
+  end
 end
+
